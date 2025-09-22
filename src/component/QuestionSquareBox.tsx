05beeb5c10e879160f8f561d81a3bf803fc9fbdc
Sameer Patel <samxpatel2@gmail.com>
2025-09-22 21:58:53 +0530

    improved box code optimize

--- src/component/QuestionSquareBox.tsx
+++ src/component/QuestionSquareBox.tsx
@@ -1,10 +1,23 @@
-export default function SquareBox(props:any){
-    return(
-        <>
-        <div onClick={props.onClick} id={props.id} className="w-4 z-40 cursor-pointer h-4 rounded-md p-3 shadow-md bg-white flex items-center justify-center">
-         {props.num}
-        </div>
-        </>
-    )
-}
+type Props = {
+  num: number;
+  state: string;
+  onClick: () => void;
+};
 
+export default function SquareBox({ num, state, onClick }: Props) {
+  let bg = "bg-gray-300"; // default notVisited
+
+  if (state === "notAnswered") bg = "bg-red-500 text-white";
+  if (state === "answered") bg = "bg-green-500 text-white";
+  if (state === "review") bg = "bg-yellow-400";
+  if (state === "answeredReview") bg = "bg-purple-500 text-white";
+
+  return (
+    <div
+      onClick={onClick}
+      className={`w-10 h-10 flex items-center justify-center rounded-md shadow cursor-pointer ${bg}`}
+    >
+      {num}
+    </div>
+  );
+}
--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -16,6 +16,24 @@ export default function Questions(props: any) {
     tags: string[];
   }
 
+
+  type Status = "notVisited" | "notAnswered" | "answered" | "review" | "answeredReview";
+
+  const [status, setStatus] = useState<Status[]>([]);
+  function markForReview(qIndex: number) {
+  setStatus((prev) => {
+    const newStatus = [...prev];
+    const current = newStatus[qIndex];
+    if (current === "answered") {
+      newStatus[qIndex] = "answeredReview";
+    } else {
+      newStatus[qIndex] = "review";
+    }
+    return newStatus;
+  });
+}
+ 
+
   const [questions, setQuestions] = useState<Question[]>([]);
   const [index, setIndex] = useState(0);
   const question = questions[index];
@@ -55,14 +73,18 @@ export default function Questions(props: any) {
       headers: { "Content-Type": "application/json" }
     })
       .then((res) => res.json())
-      .then((data) => setQuestions(data.importQuestions))
+      .then((data) =>{ setQuestions(data.importQuestions)
+      setStatus(Array(data.importQuestions.length).fill("notVisited"));
+
+      })
       .catch((err) => console.error(err));
+
   }, [props.subj]);
 
 
 
   // handle option change
-  const handleChange = (e: any, questionId: string) => {
+  const handleChange = (e: any, questionId: string ,qIndex:number) => {
     const value = e.target.value;
 
     setAnswers((prev) => {
@@ -73,11 +95,22 @@ export default function Questions(props: any) {
       } else {
         updated = prevAns.filter((v) => v !== value);
       }
+  setStatus((prev) => {
+  const newStatus = [...prev];
+  if (updated.length > 0) {
+    newStatus[qIndex] = "answered";
+  } else {
+    newStatus[qIndex] = "notAnswered";
+  }
+  return newStatus;
+});
+
+
       // console.log("in the handleCHange: ",questionId)
       return { ...prev, [questionId]: updated };
     });
   };
-     console.log(answers)
+  console.log(answers)
 
   // Submit function
   const handleSubmit = (question: Question) => {
@@ -139,30 +172,9 @@ export default function Questions(props: any) {
       console.log(e);
     }
   }
-  const btn = document.getElementById(`${index+1}`);
-  useEffect(()=>{
-    for(let i=0; i<questions.length; i++){
-      const q = questions[i];
-      const userAns:any = answers[q._id];
-      // const check = userAns[i];
 
-      // console.log("user is any wala",userAns[q._id])
-      console.log(`${i}`,userAns )
-      if(userAns){
-          
 
-         btn?.setAttribute('style', 'background-color: blue; color: white;') 
-         
-      }
-      // else if (userAns[index+1].removeItem){
-      //  btn?.setAttribute('style', 'background-color: white; color: black;') 
-      // }
-      
-    }
-  },[answers])
-  
-  
-  
+
   function submitAll() {
     for (let i = 0; i < questions.length; i++) {
       const q = questions[i];              // current question
@@ -187,118 +199,91 @@ export default function Questions(props: any) {
   // 
   return (
     <div className="w-full h-screen fixed ">
-     <div className="flex justify-center gap-2 mt-2"> 
-       
-      <div className="w-1/2">
-      <h1  className="text-2xl font-bold mb-1  ">Questions {index+1} <hr className="border-t-2 border-gray-400" /></h1>
-      <div className="h-96  overflow-y-scroll">
-      {question && (
-        <div key={question._id} className=" p-3 mb-4 ">
-          <p className="font-semibold">{question.question}</p>
-          {props.mode === "practice" && (
-            <GeminiHint
-              prompt={
-                question.question +
-                " hint with formula in 5-8 words" +
-                question.questionDiagram
-              }
-            />
-          )}
-          <img src={question.questionDiagram} alt="" />
+      <div className="flex justify-center gap-2 mt-2">
 
-          <div className="mt-2">
-         {question.option.map((opt, i) => (
- <div
-  key={i}
-  className="w-full"
->
-  <input
-    type="checkbox"
-    id={`q-${question._id}-${i}`}
-    name={`q-${question._id}`}
-    value={opt}
-    checked={answers[question._id]?.includes(opt) || false}
-    onChange={(e) => handleChange(e, question._id)}
-    className="peer hidden"
-  />
-  <label
-    htmlFor={`q-${question._id}-${i}`}
-    className="flex items-center gap-3 w-full p-4 mb-3 text-gray-700 bg-white border-2 border-gray-200 rounded-lg cursor-pointer transition-colors  hover:bg-gray-50 dark:bg-white dark:text-black dark:border-gray-500 dark:hover:bg-gray-300 peer-checked:bg-blue-300 peer-checked:border-blue-300"
-  >
-    <span className="peer-checked:text-blue-600">{opt}</span>
-  </label>
-</div>
-         ))}
-           
-          </div>
+        <div className="w-1/2">
+          <h1 className="text-2xl font-bold mb-1  ">Questions {index + 1} <hr className="border-t-2 border-gray-400" /></h1>
+          <div className="h-96  overflow-y-scroll">
+            {question && (
+              <div key={question._id} className=" p-3 mb-4 ">
+                <p className="font-semibold">{question.question}</p>
+                {props.mode === "practice" && (
+                  <GeminiHint
+                    prompt={
+                      question.question +
+                      " hint with formula in 5-8 words" +
+                      question.questionDiagram
+                    }
+                  />
+                )}
+                <img src={question.questionDiagram} alt="" />
 
-          {/* <button
+                <div className="mt-2">
+                  {question.option.map((opt, i) => (
+                    <div
+                      key={i}
+                      className="w-full"
+                    >
+                      <input
+                        type="checkbox"
+                        id={`q-${question._id}-${i}`}
+                        name={`q-${question._id}`}
+                        value={opt}
+                        checked={answers[question._id]?.includes(opt) || false}
+                        onChange={(e) => handleChange(e, question._id,index)}
+                        className="peer hidden"
+                      />
+                      <label
+                        htmlFor={`q-${question._id}-${i}`}
+                        className="flex items-center gap-3 w-full p-4 mb-3 text-gray-700 bg-white border-2 border-gray-200 rounded-lg cursor-pointer transition-colors  hover:bg-gray-50 dark:bg-white dark:text-black dark:border-gray-500 dark:hover:bg-gray-300 peer-checked:bg-blue-300 peer-checked:border-blue-300"
+                      >
+                        <span className="peer-checked:text-blue-600">{opt}</span>
+                      </label>
+                    </div>
+                  ))}
+
+                </div>
+
+                {/* <button
             className="px-4 py-2 m-2 border-[4px] hover:border-red-700"
             onClick={() => handleSubmit(question)}
           >
             Submit
           </button> */}
-        </div>
-      )}
-      </div>    
-<hr className="border-t-2 border-gray-400"></hr>
-      <button
-        disabled={index === 0}
-        onClick={() => index > 0 && setIndex(index - 1)}
-        className="border py-2 px-4 mr-1 mt-1"
-      >
-        Previous
-      </button>
-      <button
-        disabled={index === questions.length - 1}
-        onClick={() => index < questions.length - 1 && setIndex(index + 1)}
-        className="py-2 px-4 border mx-2  "
-      >
-        Next
-      </button>
+              </div>
+            )}
+          </div>
+          <hr className="border-t-2 border-gray-400"></hr>
+          <button
+            disabled={index === 0}
+            onClick={() => index > 0 && setIndex(index - 1)}
+            className="border py-2 px-4 mr-1 mt-1"
+          >
+            Previous
+          </button>
+          <button
+            disabled={index === questions.length - 1}
+            onClick={() => index < questions.length - 1 && setIndex(index + 1)}
+            className="py-2 px-4 border mx-2  "
+          >
+            Next
+          </button>
 
-      <button className="border px-4 py-2 mx-2" onClick={submitAll}  >Submit test</button>
+          <button  className="py-2 px-4 border mx-2  "onClick={()=>markForReview(index)}>Mark for review</button>
+
+          <button className="border px-4 py-2 mx-2" onClick={submitAll}  >Submit test</button>
+        </div>
+        <div className="grid grid-cols-6 gap-2 w-[30vw] h-[30vh] bg-gray-100 p-3 rounded-sm overflow-y-scroll">
+          {questions.map((_, i) => (
+            <SquareBox
+              key={i}
+              num={i + 1}
+              state={status[i]}
+              onClick={() => setIndex(i)}
+            />
+          ))}
+        </div>
       </div>
- <div className="grid  grid-cols-9 grid- auto-cols-fr  w-[30vw] h-[30vh] bg-gray-500 p-3 rounded-sm">
-  <SquareBox onClick={()=>{setIndex(0)}} num={1}   id = {1}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(1)}} num={2} id = {2}></SquareBox>
-  <SquareBox  onClick={()=>{setIndex(2)}} num={3} id = {3}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(3)}} num={4} id = {4}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(4)}} num={5} id = {5}></SquareBox>
-  <SquareBox  onClick={()=>{setIndex(5)}}  num={6} id = {6}></SquareBox>
-  <SquareBox  onClick={()=>{setIndex(6)}}  num={7} id = {7}></SquareBox>
-   <SquareBox  onClick={()=>{setIndex(7)}} num={8} id = {8}></SquareBox>
-   <SquareBox onClick={()=>{setIndex(8)}} num={9} id = {9}></SquareBox>
-   <SquareBox onClick={()=>{setIndex(9)}} num={10} id = {10}></SquareBox>
-  <SquareBox  onClick={()=>{setIndex(10)}} num={11} id = {11}></SquareBox>
-  <SquareBox  onClick={()=>{setIndex(11)}} num={12} id = {12}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(12)}} num={13} id = {13}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(13)}} num={14} id = {14}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(14)}} num={15} id = {15}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(15)}} num={16} id = {16}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(16)}} num={17} id = {17}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(17)}} num={18} id = {18}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(18)}} num={19} id = {19}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(19)}} num={20} id = {20}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(20)}} num={21} id = {21}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(21)}} num={22} id = {22}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(22)}} num={23} id = {23}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(23)}} num={24} id = {24}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(24)}} num={25} id = {25}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(25)}} num={26} id = {26}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(26)}} num={27} id = {27}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(27)}} num={28} id = {28}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(28)}} num={29} id = {29}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(29)}} num={30} id = {30}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(30)}} num={31} id = {31}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(31)}} num={32} id = {32}></SquareBox>  
-  <SquareBox onClick={()=>{setIndex(32)}} num={33} id = {33}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(33)}} num={34} id = {34}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(34)}} num={35} id = {35}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(35)}} num={36} id = {36}></SquareBox>
-  <SquareBox onClick={()=>{setIndex(36)}} num={37} id = {37}></SquareBox>
-  </div>
-    </div>
     </div>
   );
 }
