043643ed5baebfdb8e8b1b5c10e307b4627c0ff5
Sameer Patel <samxpatel2@gmail.com>
2025-10-26 18:17:20 +0530

    User icon in signup

--- src/component/UserSignin.tsx
+++ src/component/UserSignin.tsx
@@ -61,6 +61,7 @@ export default function Signin() {
           <div className="space-y-2">
             <Label htmlFor="identifier">Username or Email</Label>
             <div className="relative">
+              <User className="absolute left-3 top-2.5 h-5 w-5 text-gray-500" /> 
               <Input
                 id="identifier"
                 type="text"
